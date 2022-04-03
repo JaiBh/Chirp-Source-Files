7c6420a4ab3d9b0162bb607f85374d0b0f182da9
Jai Bhullar <81278507+JaiBh@users.noreply.github.com>
2022-04-03 22:30:42 +0100

    finished files

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// const headerDOM = document.querySelector("#header")
+
+// window.addEventListener("scroll", function(){
+//     let headerHeight = headerDOM.clientHeight + 20;
+//     let scroll = window.scrollY;
+//     if(scroll > headerHeight){
+//         console.log("hello");
+//         headerDOM.style.position = "fixed"
+//     } else{
+//         headerDOM.style.position = "static"
+//     }
+// })
+
